ac0f5f4a768be21050b403475bee7ce27a8f0ea0
Kory Kilpatrick <koryrkilpatrick@gmail.com>
2020-03-31 20:11:48 -0500

    build game dict, handle daily doubles

--- server/index.py
+++ server/index.py
@@ -1,16 +1,25 @@
 from flask import Flask
 
-
 from db import DAL 
 import db.data_models.jeopardy as jdm
+from server.convert import build_game_dict
 
 app = Flask(__name__)
+dal = DAL()
 
 @app.route('/'):
 def get_clues():  
   # can return dictionary and it will be auto jsonified by flask
-  dal.execute('select * from category order by title')
 
   dal.execute('select * from clues where category_id=%s', (category.id))
 
   dal.callproc('get_official_game_clues')
+
+def get_random_game():
+  # get J and DJ clues
+  clues = dal.callproc('build_random_game')
+  return build_game_dict(clues)
+
+def get_tiebreaker():
+  tb_clue = dal.callproc('get_random_clue', ('TB', ), one_or_none=True)
+
--- utils.py
+++ utils.py
@@ -15,3 +15,15 @@ def write_to_file(msg, err=False, warn=False, out=False):
   f = open('logs/' + filename, "a")
   f.write(msg + '\n')
   f.close()
+
+def print_daily_doubles(game_dict):
+  for game_round in ('J', 'DJ'):
+    for category_id in game_dict[game_round]:
+      for clue_id in game_dict[game_round][category_id]['clues']:
+        if game_dict[game_round][category_id]['clues'][clue_id]['is_daily_double']:
+          print(game_round, category_id, game_dict[game_round][category_id]['clues'][clue_id]['value'])
+
+
+
+
+
