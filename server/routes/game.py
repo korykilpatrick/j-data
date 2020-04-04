d38d1a91f91300d1dc6f00cc41df6e722ba7e7ca
Kory Kilpatrick <koryrkilpatrick@gmail.com>
2020-04-04 13:49:39 -0500

    /clue

--- server/routes/game.py
+++ server/routes/game.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+import random
 
 from server import app, dal
 from server.board import build_random_board, build_official_board, build_category
@@ -7,11 +8,21 @@ import db.data_models.jeopardy as jdm
 @app.route('/')
 def hello_world():
   return {
-    '/board/official': 'get a random official game board',
-    '/board/random': 'get a random board',
+    '/clue': 'get a random clue',
+    '/clue/<round>': 'get a random clue by round (J, DJ, FJ, TB)',
     '/category': 'get a random category',
-    '/clue/<round>': 'get a random clue by round (J, DJ, FJ, TB)'
+    '/board/random': 'get a random board',
+    '/board/official': 'get a random official game board',
   }
+
+@app.route('/clue')
+@app.route('/clue/<game_round>', methods=['GET'])
+def get_random_clue(game_round=None):
+  if not game_round:
+    game_round = random.choice(['J', 'DJ', 'FJ', 'TB'])
+
+  clue = dal.callproc('get_random_clue', (game_round, ), one_or_none=True)
+  return build_category([clue])
 
 @app.route('/board/official', methods=['GET'])
 def get_official_board():  
@@ -29,8 +40,4 @@ def get_random_category():
   clues = dal.callproc('get_random_category')
   return build_category(clues)
 
-@app.route('/clue/<game_round>', methods=['GET'])
-def get_random_clue(game_round):
-  clue = dal.callproc('get_random_clue', (game_round, ), one_or_none=True)
-  return build_category([clue])
 
