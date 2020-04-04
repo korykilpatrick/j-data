7c0bdce8b6f1ce8edc6bdfd661bcbcca9f29db9c
Kory Kilpatrick <koryrkilpatrick@gmail.com>
2020-04-03 20:53:57 -0500

    fml

--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 
-# from db import DAL
+from db import DAL
 
 app = Flask(__name__)
 CORS(app)
 
-# dal = DAL()
+dal = DAL()
 
 from server.routes import game, user, auth
